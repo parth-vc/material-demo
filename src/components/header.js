d3035a3a935324ef80ce8c468815eb088b3b1de6
parth-vc <parth.paun@viitorcloud.in>
2020-04-09 16:21:34 +0530

    sidebar changes

--- src/components/header.js
+++ src/components/header.js
@@ -16,11 +16,22 @@ import Badge from "@material-ui/core/Badge";
 import IconButton from "@material-ui/core/IconButton";
 import NotificationsNoneOutlinedIcon from "@material-ui/icons/NotificationsNoneOutlined";
 import EmailOutlinedIcon from "@material-ui/icons/EmailOutlined";
+import Collapse from '@material-ui/core/Collapse';
+import ExpandLess from '@material-ui/icons/ExpandLess';
+import ExpandMore from '@material-ui/icons/ExpandMore';
+import StarBorder from '@material-ui/icons/StarBorder';
+import ShoppingCartOutlinedIcon from '@material-ui/icons/ShoppingCartOutlined';
+import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import Avatar from "@material-ui/core/Avatar";
+import HomeIcon from '@material-ui/icons/Home';
+import DescriptionIcon from '@material-ui/icons/Description';
 const drawerWidth = 240;
 const useStyles = makeStyles(theme => ({
   grow: {
     flexGrow: 1
+  },
+  nested: {
+    paddingLeft: theme.spacing(5),
   },
   appBar: {
     zIndex: theme.zIndex.drawer + 1
@@ -38,6 +49,15 @@ const useStyles = makeStyles(theme => ({
 }));
 export default function Header() {
   const classes = useStyles();
+  const [open, setOpen] = React.useState(false);
+  const [useropen,setUseropen]=React.useState(false)
+  const handleUser = () =>{
+    setUseropen(!useropen)
+  }
+  const handleClick = () => {
+    setOpen(!open);
+    
+  };
   return (
     <>
       <CssBaseline />
@@ -69,18 +89,57 @@ export default function Header() {
         }}
       >
         <Toolbar />
+
         <div className={classes.drawerContainer}>
-          <List>
-            {["Inbox", "Starred", "Send email", "Drafts"].map((text, index) => (
-              <ListItem button key={text}>
-                <ListItemIcon>
-                  {index % 2 === 0 ? <InboxIcon /> : <MailIcon />}
-                </ListItemIcon>
-                <ListItemText primary={text} />
-              </ListItem>
-            ))}
-          </List>
-          <Divider />
+      <List className={classes.root}>
+      <ListItem button>
+        <ListItemIcon>
+          <HomeIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+      <ListItem button onClick={handleClick}>
+        <ListItemIcon>
+          <ShoppingCartOutlinedIcon />
+        </ListItemIcon>
+        <ListItemText primary="Ecommerce" />
+        {open ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={open} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Starred" />
+          </ListItem>
+        </List>
+      </Collapse>
+      <ListItem button onClick={handleUser}>
+        <ListItemIcon>
+          <AccountCircleIcon />
+        </ListItemIcon>
+        <ListItemText primary="User" />
+        {useropen ? <ExpandLess /> : <ExpandMore />}
+      </ListItem>
+      <Collapse in={useropen} timeout="auto" unmountOnExit>
+        <List component="div" disablePadding>
+          <ListItem button className={classes.nested}>
+            <ListItemIcon>
+              <StarBorder />
+            </ListItemIcon>
+            <ListItemText primary="Starred" />
+          </ListItem>
+        </List>
+      </Collapse>
+      <ListItem button>
+        <ListItemIcon>
+          <DescriptionIcon />
+        </ListItemIcon>
+        <ListItemText primary="Dashboard" />
+      </ListItem>
+    </List>        
+  <Divider />
           <List>
             {["All mail", "Trash", "Spam"].map((text, index) => (
               <ListItem button key={text}>
